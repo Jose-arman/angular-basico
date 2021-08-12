2eacdd218b6d1e871c1452972f42d5f4651d1cd0
Jose-arman <Sanjuanluisjosearmando@gmail.com>
2021-08-11 22:03:19 -0500

    primer commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+//import { contadorcomponet } from './contador.component';
+import { Component } from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -19,7 +21,7 @@ describe('AppComponent', () => {
   it(`should have as title 'bases'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('bases');
+    //expect(app.titulo).toEqual('bases');
   });
 
   it('should render title', () => {
---
+++ src/app/contador/contador/contador.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { contadorcomponet } from './contador.component';
+
+
+@NgModule({
+    declarations: [
+        contadorcomponet
+    ],
+    exports: [
+        contadorcomponet
+    ]
+})
+export class contadorModule {}
